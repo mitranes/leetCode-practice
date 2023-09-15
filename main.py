2cc9ecabbaacf49300e397025b906abeeae8df68
Mitra Hendy <mitranes@gmail.com>
2023-09-15 14:24:57 -0700

    Code for LeetCode threeSum Problem

---
+++ main.py
@@ -0,0 +1,31 @@
+import numpy as np
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def threeSum(nums):
+    myList = []
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            for k in range(j+1,len(nums)):
+                if nums[i]+nums[j]+nums[k] == 0:
+                    list = [nums[i], nums[j], nums[k]]
+                    myList.append(list)
+    return hashDups(myList)
+
+def hashDups(lists):
+    myDict = {}
+    for i in range(len(lists)):
+        newTuple = tuple(sorted(lists[i]))
+        myDict.update({newTuple: i})
+    print(myDict)
+    finalList = myDict.keys()
+    return finalList
+
+
+
+if __name__ == '__main__':
+    practice = [-1,0,1,2,-1,-4,-2,-3,3,0,4]
+    print(threeSum(practice))
